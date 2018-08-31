c9b99ff7318f2b2e21e1cedb01dd3b41294b8be2
Gabriela Suriano <surianogabriela@gmail.com>
2018-08-31 08:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function shout(string) {
-  return 
-  string.toUpperCase()
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return string.toUpperCase()
 }
