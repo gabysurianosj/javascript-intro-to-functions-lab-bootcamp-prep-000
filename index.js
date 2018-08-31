3261bc19667db7221c9a33ff22093045dc5b8bab
Gabriela Suriano <surianogabriela@gmail.com>
2018-08-31 08:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,3 @@ function sayHiToGrandma(string) {
   if (string === "hello") 
   return "I can't hear you!"
 }
-function sayHiToGrandma(string) {
-  if (string === "HELL0")
-  return "YES INDEED!"
-}
