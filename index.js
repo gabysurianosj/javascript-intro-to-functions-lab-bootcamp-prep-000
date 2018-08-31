338bd407edf6b1905e13adb168f3f62a010e0e9d
Gabriela Suriano <surianogabriela@gmail.com>
2018-08-31 08:17:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase()) 
 }
-function logShout(string) {
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  
+}
